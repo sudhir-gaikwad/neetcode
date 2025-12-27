1151908fba379276c36ad2b25c47d0ade8a794ae
Sudhir <ssudhir.gaikwad@gmail.com>
2025-12-27 15:27:22 +0530

    corrections

--- Two Pointer Sliding Window/Set 1/27. Remove Element/Solution.java
+++ Two Pointer Sliding Window/Set 1/27. Remove Element/Solution.java
@@ -1,36 +1,34 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        if (nums.length == 0) return 0;
         int left = 0;
-        int count = 0;
         for (int right = 0; right < nums.length; right++) {
-            System.out.println(nums[left] + " " + nums[right]);
-            if (nums[left] == val) {
-                if (val != nums[right]) {
-                    nums[left] = nums[right];
-                    nums[right] = val;
-                    left++;
-                    count++;
-                    // System.out.println("-->> " + Arrays.toString(nums));
-                }
-            } else {
+            if (nums[right] != val) {
+                nums[left] = nums[right];
                 left++;
-                count++;
             }
         }
-        return count;
+        return left;
     }
 }
 
 // class Solution {
 //     public int removeElement(int[] nums, int val) {
-//         int i, j;
-//         for (i = j = 0; j < nums.length; j++) {
-//             if (nums[j] != val) {
-//                 nums[i]=nums[j];
-//                 i++;
+//         if (nums.length == 0) return 0;
+//         int left = 0;
+//         int count = 0;
+//         for (int right = 0; right < nums.length; right++) {
+//             if (nums[left] == val) {
+//                 if (nums[right] != val) {
+//                     nums[left] = nums[right];
+//                     nums[right] = val;
+//                     left++;
+//                     count++;
+//                 }
+//             } else {
+//                 left++;
+//                 count++;
 //             }
 //         }
-//         return i;
+//         return count;
 //     }
 // }
--- Two Pointer Sliding Window/Set 1/28. Find the Index of the First Occurrence in a String/Solution.java
+++ Two Pointer Sliding Window/Set 1/28. Find the Index of the First Occurrence in a String/Solution.java
@@ -1,26 +1,16 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        char hay[] = haystack.toCharArray();
-        char need[] = needle.toCharArray();
-        int index = 0;
-        int i = 0, j = 0;
-        while (i < hay.length) {
-            System.out.println("i=" + i + " j=" + j + " Index=" + index);
-            if (hay[i] == need[j]) {
-                System.out.println("====== i=" + i + " j=" + j);
-                if (j == (need.length - 1)) {
-                    System.out.println("Returnnnnnn.. : " + index);
-                    return index;
-                }
+        int n = haystack.length();
+        int m = needle.length();
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
                 j++;
-                i++;
-            } else {
-                index++;
-                i = index;
-                j = 0;
-                System.out.println("Index:: " + index);
             }
-
+            // If the entire needle was matched (j reached needle length)
+            if (j == m) {
+                return i;
+            }
         }
         return -1;
     }
@@ -28,10 +18,22 @@ class Solution {
 
 // class Solution {
 //     public int strStr(String haystack, String needle) {
-//         if (needle.isEmpty()) return 0;
-//         int n = haystack.length(), m = needle.length();
-//         for (int i = 0; i <= n - m; i++) {
-//             if (haystack.substring(i, i + m).equals(needle)) return i;
+//         char hay[] = haystack.toCharArray();
+//         char need[] = needle.toCharArray();
+//         int index = 0;
+//         int i = 0, j = 0;
+//         while (i < hay.length) {
+//             if (hay[i] == need[j]) {
+//                 if (j == (need.length - 1)) {
+//                     return index;
+//                 }
+//                 j++;
+//                 i++;
+//             } else {
+//                 index++;
+//                 i = index;
+//                 j = 0;
+//             }
 //         }
 //         return -1;
 //     }
